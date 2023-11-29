57670fc34627ae7d2a6d2cd7fc72b99e81bc1d6c
vaibhavd-github <vaibhavdukle1622@gmail.com>
2023-11-29 17:26:00 +0530

    Modify Snowutils

--- src/jobs/utils/job1copy.py
+++ src/jobs/utils/job1copy.py
@@ -1,6 +1,7 @@
 import boto3
 import sys
 
+
 # print("Before Append:" ,sys.path)
 # print("====================================")
 # sys.path.append('c:\\Users\\Shree\\bw-dse-retail-analytics\\src\\')
@@ -9,6 +10,7 @@ import sys
 # print("====================================")
 from vaultUtils import VaultClient
 from awsUtils import AWSConnector 
+from snowflake import SnowflakeConnector
  
 VAULT_URL = "http://127.0.0.1:8200" 
 ROLE_ID = "865df005-e0c3-42c9-8ceb-883f3e88c8fd"
@@ -46,3 +48,45 @@ response = iam_client.list_groups()
 
 # response = client.list_groups()
 print("IAM Groups:", response)
+
+
+# Replace these with your Snowflake account details
+account = 'latflkz-icb55504'
+
+SECRET_PATH = "secret/data/aws"
+
+vault_client = VaultClient(VAULT_URL, ROLE_ID, SECRET_ID, SECRET_PATH)
+token = vault_client.authenticate_with_approle()
+
+if token:
+    secret_data = vault_client.get_secret(token)
+    if secret_data:
+        print("Secret data:", secret_data)
+    else:
+        print("Failed to retrieve secret.")
+else:
+    print("Failed to authenticate with AppRole.")
+
+user = secret_data['data']['bw-snow-serviceusername-dev']
+password = secret_data['data']['bw-snow-serviceuserpassword-dev']
+
+# user = 'VAIBHAVSNOWFLAKE'
+# password = 'Hellow@123'
+warehouse = 'COMPUTE_WH'
+database = 'SNOWFLAKE_SAMPLE_DATA'
+schema = 'TPCH_SF1'
+
+# Create an instance of SnowflakeConnector
+snowflake_conn = SnowflakeConnector(account, user, password, warehouse, database, schema)
+
+# Connect to Snowflake
+snowflake_conn.connect()
+
+# Execute a query
+query_result = snowflake_conn.execute_query("SELECT * from SUPPLIER limit 5")
+
+# Print the result
+print("Query Result:", query_result)
+
+# Close the connection
+snowflake_conn.close_connection()
